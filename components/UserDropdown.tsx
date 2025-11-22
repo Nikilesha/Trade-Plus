91c8cb89139285ade0e010ea2b128a17ed270be0
Nikilehsa <nikilesha.nivas@gmail.com>
2025-11-22 23:49:55 +0530

    fixed bugs

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -25,14 +25,14 @@ const UserDropdown = () => {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="flex items-center gap-3 mb-5 text-gray-4 hover:text-yellow-500">
+                <Button variant="ghost" className="flex items-center gap-3 mb-5 text-gray-400 hover:text-yellow-500">
                     <Avatar className="h-8 w-8">
                         <AvatarImage src = "https://github.com/shadcn.png"></AvatarImage>
                         <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                             {user.name[0]}
                         </AvatarFallback>
                     </Avatar>
-                    <div className="hidden md-flex flex-col items-start">
+                    <div className="hidden md:flex flex-col items-start">
                         <span className="text-base font-medium text-gray-400">
                             {user.name}
                         </span>
@@ -58,12 +58,12 @@ const UserDropdown = () => {
                     </div>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator className="bg-gray-600" />
-                <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-md font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
+                <DropdownMenuItem onClick={handleSignOut} className="text-gray-100 text-base font-medium focus:bg-transparent focus:text-yellow-500 transition-colors cursor-pointer">
                     <LogOut className="h-4 w-4 mr-2 hidden sm:block" />
                     Logout
                 </DropdownMenuItem>
 
-                <DropdownMenuSeparator className="hidden sm:blockbg-gray-600" />
+                <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
 
                 <nav className="sm:hidden">
                     <NavItems />
